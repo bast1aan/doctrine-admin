eb2c4305993330aeafe8591cb13ec8d6f223cdb0
Bastiaan Welmers <bastiaan@welmers.net>
2013-12-06 18:40:55 +0100

    Added corresponding populator for the html form (not tested)

--- src/Bast1aan/DoctrineAdmin/View/Form.php
+++ src/Bast1aan/DoctrineAdmin/View/Form.php
@@ -37,5 +37,41 @@ namespace Bast1aan\DoctrineAdmin\View {
 		public function getEntity() {
 			return $this->entity;
 		}
+		
+		/**
+		 * @param array|string[] $formData
+		 * @todo excerpt. needs to be tested and improved
+		 */
+		public function populate(array $formData) {
+			$da = $this->entity->getDoctrineAdmin();
+			foreach($this->entity->getFieldNames() as $fieldName) {
+				$property = $this->entity->getColumn($fieldName);
+				if (isset($formData[$fieldName])) {
+					$property->setValueFromString($formData[$fieldName]);
+				} else {
+					$property->setValue(null);
+				}
+				$this->entity->setColumn($property);
+			}
+			
+			foreach($this->entity->getAssociationNames() as $associationName) {
+				$property = $this->entity->getColumn($fieldName);
+				if ($property instanceof DoctrineAdmin\CollectionAssociationProperty) {
+					$property->clear();
+					
+					if (isset($formData[$associationName])) {
+						foreach((array) $formData[$associationName] as $id) {
+							$property->add($da->find($property->getType(), $id));
+						}
+					}
+				} elseif ($property instanceof DoctrineAdmin\AssociationProperty) {
+					if (isset($formData[$associationName])) {
+						$property->setValue($da->find($property->getType(), $id));
+					} else {
+						$property->setValue(null);
+					}
+				}
+			}
+		}
 	}
 }
